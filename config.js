76b77a56a1f88662cdba8933e250dc5352e01883
sparrow9616 <108956698+sparrow9616@users.noreply.github.com>
2024-12-14 20:09:14 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,13 @@
+const CONFIG = {
+    // List of active bots - Add your bot usernames here
+    BOTS: [
+        "your_main_bot",
+        "your_backup_bot"
+    ],
+    // Default bot to use if no others are available
+    DEFAULT_BOT: "your_main_bot",
+    // Delay before redirect (in milliseconds)
+    REDIRECT_DELAY: 1000,
+    // Debug mode - set to true to see console logs
+    DEBUG: false
+};
