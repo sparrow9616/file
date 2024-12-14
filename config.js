c8731d0e0f0671eaca4edb1246fa157f381a9874
sparrow9616 <108956698+sparrow9616@users.noreply.github.com>
2024-12-14 20:11:18 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 const CONFIG = {
     // List of active bots - Add your bot usernames here
     BOTS: [
-        "your_main_bot",
+        "fileshare9616_bot",
         "your_backup_bot"
     ],
     // Default bot to use if no others are available
-    DEFAULT_BOT: "your_main_bot",
+    DEFAULT_BOT: "fileshare9616_bot",
     // Delay before redirect (in milliseconds)
     REDIRECT_DELAY: 1000,
     // Debug mode - set to true to see console logs
